dc67f19a023fdb5a1dc04fc71d21f97e95f88366
Ejbert <ebertelsen2015@kellogg.northwestern.edu>
2015-02-19 16:23:27 -0600

    Updated User Resource
    Added Create New User page
    Added all 7 actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,25 +6,40 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id:params["id"])
-    # EB says: there might be a more elegant way to do this, but it seems to work
+    # EB says: there might be a more elegant way to do this using has_many or belongs_to, but it seems to work
     @team_members = Team_Member.where(user_id: @user.id)
     @reviews = Review.where(team_member_id: @team_members)
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    user_params = params.require(:user).permit!
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render text: "FAIL"
+    end
   end
 
   def edit
-    @user = User.find_by(id:params["id"])
+    @user = User.find_by(id: params["id"])
   end
 
   def update
+    user_params = params.require(:user).permit!
+    @user = User.find_by(id: params["id"])
+    @user.update(user_params)
+    redirect_to users_path
   end
 
   def destroy
+    @user = User.find_by(id: params["id"])
+    @user.destroy
+    redirect_to users_path
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :team_members
-  has_many :reviews, through: :team_members
+  has_many :reviews, through: :team_members, dependent: :destroy
   has_many :teams, through: :team_members
 end
